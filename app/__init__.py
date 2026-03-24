de0fe1373d95fdd5356c882ff56b36fc0bcbbedc
Muneeb Arif <muneebarif@Macbooks-MacBook-Pro-2.local>
2026-03-24 23:44:05 +0500

    updated cors

--- app/__init__.py
+++ app/__init__.py
@@ -27,6 +27,7 @@ def create_app(config_class=Config):
         "http://localhost:5173",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
+        "https://app.informityx.ai"
     ]
     CORS(app, 
          resources={r"/*": {
